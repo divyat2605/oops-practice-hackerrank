a71843720402f7c17ce6b40cf50f2910e953d4d0
divi2605 <divya.tripathi.2605@gmail.com>
2026-05-09 02:57:32 +0530

    feat: added student class solution using getters and setters in cpp

---
+++ student-class-cpp/README.md
@@ -0,0 +1,33 @@
+# Student Class in C++
+
+This project demonstrates basic Object-Oriented Programming concepts in C++ using a `Student` class.
+
+## Concepts Used
+- Classes and Objects
+- Private and Public Access Specifiers
+- Getter and Setter Functions
+- Stringstream
+- Encapsulation
+
+## Features
+The `Student` class stores:
+- Age
+- First Name
+- Last Name
+- Standard
+
+It also includes:
+- Getter functions
+- Setter functions
+- `to_string()` method
+
+## Sample Output
+
+15
+carmack, john
+10
+
+15,john,carmack,10
+
+## Platform
+Solved on HackerRank.
---
+++ student-class-cpp/student.cpp
@@ -0,0 +1,71 @@
+// HackerRank C++ Class Problem Solution
+// Concepts: OOP, Classes, Getters/Setters, Stringstream
+
+#include <iostream>
+#include <sstream>
+using namespace std;
+/*
+Enter code for class Student here.
+Read statement for specification.*/
+class Student{
+    private:
+    int age, standard;
+    string first_name, last_name;
+    
+    public:
+    void set_age(int a){
+        age =a;
+    }
+    void set_standard(int b){
+        standard=b;
+    }
+    void set_first_name(string fs){
+        first_name = fs;
+    }
+    void set_last_name(string ls){
+        last_name = ls;
+    }
+    int get_age(){
+        return age;
+
+    }
+    int get_standard(){
+        return standard;
+    }
+    string get_first_name(){
+        return first_name;
+    }
+    string get_last_name(){
+        return last_name;
+    }
+    // string fxn
+    string to_string(){
+        stringstream ss;
+        ss<<age <<","<<first_name<<","<<last_name<<","<<standard;
+        return ss.str();
+    }
+};
+
+
+
+
+int main() {
+    int age, standard;
+    string first_name, last_name;
+    
+    cin >> age >> first_name >> last_name >> standard;
+    
+    Student st;
+    st.set_age(age);
+    st.set_standard(standard);
+    st.set_first_name(first_name);
+    st.set_last_name(last_name);
+    
+    cout << st.get_age() << "\n";
+    cout << st.get_last_name() << ", " << st.get_first_name() << "\n";
+    cout << st.get_standard() << "\n";
+    cout << "\n";
+    cout << st.to_string();
+    
+    return 0;
+}
